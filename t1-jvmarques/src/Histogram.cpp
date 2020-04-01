3a7c7078bd42fe322f584882d7fd8841ce8b5e4d
João Bolsson <joaovictorbolsson@gmail.com>
2020-04-01 17:04:23 -0300

    plotting  histogram

--- t1-jvmarques/src/Histogram.cpp
+++ t1-jvmarques/src/Histogram.cpp
@@ -1,5 +1,6 @@
 #include "Histogram.h"
 #include "gl_canvas2d.h"
+#include "Point.h"
 
 Histogram::Histogram(Image *img, int x, int y, unsigned width, unsigned height) {
     this->x = x;
@@ -7,6 +8,44 @@ Histogram::Histogram(Image *img, int x, int y, unsigned width, unsigned height) 
     this->width = width;
     this->height = height;
     this->img = img;
+    numMaxPixels = img->getHeight() * img->getWidth();
+    init();
+}
+
+void Histogram::init() {
+    Pixel **pixels = img->getPixels();
+
+    for (int y = 0; y < img->getHeight(); y++) {
+        for (int x = 0; x < img->getWidth(); x++) {
+            Pixel p = pixels[y][x];
+            int r = p.getRed() * 255;
+            int g = p.getGreen() * 255;
+            int b = p.getBlue() * 255;
+
+            // never must happen, but just to avoid index out of bounds anyway
+            if (r >= 0 && r < ARRAY_CHANNEL_SIZE) {
+                red[r]++;
+            }
+
+            if (g >= 0 && g < ARRAY_CHANNEL_SIZE) {
+                green[r]++;
+            }
+
+            if (b >= 0 && b < ARRAY_CHANNEL_SIZE) {
+                blue[r]++;
+            }
+        }
+    }
+}
+
+void Histogram::plotChannel(int *channel, unsigned size) {
+    for (int i = 0; i < size; i++) {
+        int coordX = i * width / (ARRAY_CHANNEL_SIZE - 1);
+        int coordY = channel[i] * height / numMaxPixels;
+
+        rect(this->x + coordX, this->y, this->x + coordX, this->y + coordY);
+        // point(this->x + coordX, this->y + coordY);
+    }
 }
 
 void Histogram::render() {
@@ -15,9 +54,12 @@ void Histogram::render() {
     line(x, y, x + width, y);  // x axis
     rect(x, y, x, y + height); // y axis
 
-    // Pixel **pixels = img->getPixels();
-    // for (int y = 0; y < height; y++) {
-    //     for (int x = 0; x < width; x++) {
-    //     }
-    // }
+    color(1, 0, 0);
+    plotChannel(red, ARRAY_CHANNEL_SIZE);
+
+    color(0, 1, 0);
+    plotChannel(green, ARRAY_CHANNEL_SIZE);
+
+    color(0, 0, 1);
+    plotChannel(blue, ARRAY_CHANNEL_SIZE);
 }
--- t1-jvmarques/src/Histogram.h
+++ t1-jvmarques/src/Histogram.h
@@ -3,16 +3,23 @@
 
 #include "Image.h"
 
+#define ARRAY_CHANNEL_SIZE 256
+
 class Histogram {
 protected:
     int x, y;
-    unsigned width, height;
+    unsigned width, height, numMaxPixels;
+    int red[ARRAY_CHANNEL_SIZE] = {0};
+    int green[ARRAY_CHANNEL_SIZE] = {0};
+    int blue[ARRAY_CHANNEL_SIZE] = {0};
 
 private:
     Image *img;
 
 public:
     Histogram(Image *img, int x, int y, unsigned width, unsigned height);
+    void init();
+    void plotChannel(int *vect, unsigned size);
     void render();
 };
 
