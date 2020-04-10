55ae239419cf43a5e9f5e4f410f771443e8b5010
João Bolsson <joaovictorbolsson@gmail.com>
2020-04-10 20:38:56 -0300

    chaging controls

--- t2-jvmarques/src/Constants.h
+++ t2-jvmarques/src/Constants.h
@@ -1,10 +1,10 @@
 #ifndef __CONSTANTS_H__
 
-#define KEY_4_LINE 49
-#define KEY_4_RECTANGLE 50
-#define KEY_4_CURVE 51
-#define KEY_4_ROTATE_RIGHT 52
-#define KEY_4_ROTATE_LEFT 53
-#define KEY_4_CLEAR 54
+#define KEY_4_LINE '1'
+#define KEY_4_RECTANGLE '2'
+#define KEY_4_CURVE '3'
+#define KEY_4_ROTATE_RIGHT 'r'
+#define KEY_4_ROTATE_LEFT 'l'
+#define KEY_4_CLEAR 'c'
 
 #endif
